5f2e018e43c5044a3f75ba42b5327007201ad178
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-09-27 07:17:23 -0400

    Requests Server-Database: Done

--- server/routes/api-routes-insert.js
+++ server/routes/api-routes-insert.js
@@ -3,4 +3,22 @@ const models = require('../models') // DB's models
 const db = require("../models");
 
 module.exports = function (app) {
+    app.post("api/add/player", (req, res, next) => {
+        db.player.findAll({ where: req.body })
+            .then(data => {
+                if (data.length == 0) {
+                    db.player.create(d)
+                        .then(data => res.status(200).json({ msg: "Insertion Successful!" }))
+                        .catch(next)
+                }
+                else res.status(200).json({ msg: "Player Exists, No Insertion!" })
+            })
+            .catch(err => next(err))
+    })
+
+    app.post("api/add/score", (req, res, next) => {
+        db.score.create(req.body)
+            .then(data => res.status(200).json({ msg: "Insertion Successful!" }))
+            .catch(next)
+    })
 }
--- server/routes/api-routes-read.js
+++ server/routes/api-routes-read.js
@@ -3,5 +3,41 @@ const models = require('../models') // DB's models
 const db = require("../models");
 
 module.exports = function (app) {
+    app.get("/api/get/players", (req, res, next) => {
+        db.player.findAll()
+            .then(data => {
+                res.status(200).json(data)
+            })
+            .catch(next)
+    })
 
+    app.get("/api/get/location/infos", (req, res, next) => {
+        db.location.findAll({
+            include: [
+                {
+                    model: db.loc_dealer,
+                    include: [
+                        {
+                            model: db.dealer,
+                            attributes: ['id', 'name']
+                        }
+                    ]
+                },
+                {
+                    model: db.loc_player,
+                    include: [
+                        {
+                            model: db.player,
+                            attributes: ['id', 'name']
+                        },
+                        { model: db.score }
+                    ]
+                }
+            ]
+        })
+            .then(data => {
+                res.status(200).json(data)
+            })
+            .catch(next)
+    })
 }
